7650c4daf0ad2f20868415162785037a9d2021af
Brooke Kuhlmann <brooke@alchemists.io>
2020-07-13 20:28:46 -0600

    Fixed Rubocop Lint/NonDeterministicRequireOrder issues.
    Sorting ensures required files are consistently ordered to avoid hard
    to debug issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,8 @@ require "pry"
 require "pry-byebug"
 require "git/lint"
 
-Dir[File.join(__dir__, "support", "shared_contexts", "**/*.rb")].each(&method(:require))
-Dir[File.join(__dir__, "support", "shared_examples", "**/*.rb")].each(&method(:require))
+Dir[File.join(__dir__, "support", "shared_contexts", "**/*.rb")].sort.each(&method(:require))
+Dir[File.join(__dir__, "support", "shared_examples", "**/*.rb")].sort.each(&method(:require))
 
 # Ensure CI environments are disabled for local testing purposes.
 ENV["CIRCLECI"] = "false"
