a299596e7de90776bd6e86a1178e61458c91d1c3
Brooke Kuhlmann <brooke@alchemists.io>
2020-10-10 16:44:19 -0600

    Removed Lint/RedundantCopDisableDirective false positive
    No longer needed now that Rubocop 0.93.0 has fixed this error.

--- lib/git/lint/parsers/trailers/collaborator.rb
+++ lib/git/lint/parsers/trailers/collaborator.rb
@@ -7,7 +7,6 @@ module Git
         class Collaborator
           DEFAULT_KEY_PATTERN = /\ACo.*Authored.*By.*\Z/i.freeze
 
-          # rubocop:disable Lint/MixedRegexpCaptureTypes
           DEFAULT_MATCH_PATTERN = /
             (?<key>\A.+)         # Key (anchored to start of line).
             (?<delimiter>:)      # Key delimiter.
@@ -17,7 +16,6 @@ module Git
             (?<email><.+>)?      # Collaborator email (optional).
             \Z                   # End of line.
           /x.freeze
-          # rubocop:enable Lint/MixedRegexpCaptureTypes
 
           def initialize text,
                          key_pattern: DEFAULT_KEY_PATTERN,
