6cb305b4bf7a49b9f394ccc7f16b7892188d2d57
Brooke Kuhlmann <brooke@alchemists.io>
2020-11-01 06:16:06 -0700

    Added version release notes

--- lib/git/lint/identity.rb
+++ lib/git/lint/identity.rb
@@ -6,7 +6,7 @@ module Git
     module Identity
       NAME = "git-lint"
       LABEL = "Git Lint"
-      VERSION = "1.1.1"
+      VERSION = "1.2.0"
       VERSION_LABEL = "#{LABEL} #{VERSION}"
     end
   end
