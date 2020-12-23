bce2fbe8acb367feab91150940342c9c53dd6926
Brooke Kuhlmann <brooke@alchemists.io>
2020-12-23 14:47:30 -0700

    Refactored branch reporter to list private methods in call order
    Minor clean up so methods read top to bottom in terms of call order.

--- lib/git/lint/reporters/branch.rb
+++ lib/git/lint/reporters/branch.rb
@@ -23,20 +23,28 @@ module Git
 
         attr_reader :collector, :colorizer
 
-        def commit_report
-          collector.to_h.reduce "" do |details, (commit, analyzers)|
-            details + Commit.new(commit: commit, analyzers: analyzers).to_s
-          end
-        end
-
         def branch_report
           return "" unless collector.issues?
 
           "\n\n#{commit_report}".chomp "\n"
         end
 
+        def commit_report
+          collector.to_h.reduce "" do |details, (commit, analyzers)|
+            details + Commit.new(commit: commit, analyzers: analyzers).to_s
+          end
+        end
+
         def commit_total
           %(#{"commit".pluralize count: collector.total_commits} inspected)
+        end
+
+        def issue_totals
+          if collector.issues?
+            "#{issue_total} detected (#{warning_total}, #{error_total})"
+          else
+            colorizer.green("0 issues") + " detected"
+          end
         end
 
         def issue_total
@@ -52,14 +60,6 @@ module Git
         def error_total
           color = collector.errors? ? :red : :green
           colorizer.public_send color, "error".pluralize(count: collector.total_errors)
-        end
-
-        def issue_totals
-          if collector.issues?
-            "#{issue_total} detected (#{warning_total}, #{error_total})"
-          else
-            colorizer.green("0 issues") + " detected"
-          end
         end
       end
     end
