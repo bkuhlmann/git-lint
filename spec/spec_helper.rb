8fdb2e14b88d78b0807b2f5b1d5660c3ceb415bb
Brooke Kuhlmann <brooke@alchemists.io>
2021-02-13 11:35:06 -0700

    Added Ruby garbage collection compaction
    Necessary to leverage Ruby 3.0.0 automatic garbage collection
    compaction. The first line enables automatic compaction while the
    second line ensures the current application is compatible with
    automatic compaction.
    
    You'll want to look for crash ouput similar to `ruby -e
    "Process.kill(:SEGV, $$)"` and unexpected TypeErrors. Further details
    can be found [here](https://alanwu.space/post/check-compaction).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ SimpleCov.start { enable_coverage :branch }
 require "git/lint"
 require "refinements"
 
+GC.auto_compact = true
+GC.verify_compaction_references double_heap: true, toward: :empty
+
 using Refinements::Pathnames
 
 require "git_plus/spec/shared_contexts/temp_dir"
