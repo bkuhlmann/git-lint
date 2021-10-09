289cee19e4b52bcf97d67a5ebf5205c39887f3b6
Brooke Kuhlmann <brooke@alchemists.io>
2021-10-09 15:49:33 -0600

    Added CLI config action
    Necessary to manage the gem configuration as a command line action.

---
+++ lib/git/lint/cli/actions/config.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module Git
+  module Lint
+    module CLI
+      module Actions
+        # Handles gem configuration action.
+        class Config
+          def initialize configuration: Configuration::Loader::HANDLER, container: Container
+            @configuration = configuration
+            @container = container
+          end
+
+          def call action
+            case action
+              when :edit then edit
+              when :view then view
+              else logger.error { "Invalid configuration action: #{action}." }
+            end
+          end
+
+          private
+
+          attr_reader :configuration, :container
+
+          def edit = kernel.system("$EDITOR #{configuration.current}")
+
+          def view = kernel.system("cat #{configuration.current}")
+
+          def kernel = container[__method__]
+
+          def logger = container[__method__]
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/git/lint/cli/actions/config_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Git::Lint::CLI::Actions::Config do
+  subject(:action) { described_class.new }
+
+  include_context "with application container"
+
+  describe "#call" do
+    it "edits configuration" do
+      action.call :edit
+      expect(kernel).to have_received(:system).with(include("EDITOR"))
+    end
+
+    it "views configuration" do
+      action.call :view
+      expect(kernel).to have_received(:system).with(include("cat"))
+    end
+
+    it "logs error with invalid action" do
+      result = proc { action.call :bogus }
+      expect(&result).to output("Invalid configuration action: bogus.\n").to_stdout
+    end
+  end
+end
