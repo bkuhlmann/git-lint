eb4218ff046aa4f8323a111678850722994f364b
Brooke Kuhlmann <brooke@alchemists.io>
2021-11-12 09:24:04 -0700

    Added configuration setting for analyzers
    Necessary to encapsulate each setting for an analyzer. This will
    provide a common settings interface for all analyzers to work with so
    we no longer have to rely on a primitive hash. This will soon replace
    the defaults (class method) currently defined on each analyzer.

---
+++ spec/lib/git/lint/configuration/setting_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe Git::Lint::Configuration::Setting do
+  subject(:content) { described_class.new }
+
+  describe "#initialize" do
+    let :proof do
+      {
+        enabled: nil,
+        excludes: nil,
+        id: nil,
+        includes: nil,
+        maximum: nil,
+        minimum: nil,
+        severity: nil
+      }
+    end
+
+    it "answers default attributes" do
+      expect(content).to have_attributes(proof)
+    end
+
+    it "fails when attempting to modify a frozen attribute" do
+      expecation = proc { content.enabled = true }
+      expect(&expecation).to raise_error(FrozenError)
+    end
+  end
+end
