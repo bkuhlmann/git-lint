8a887ac13ec8fb402d63491d07891f792d4f8b80
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:33:50 -0700

    Added commit trailer signer key analyzer
    Necessary to validate the trailer key used for authors who sign a
    commit. This is similar in behavior to Co-Authored-By trailers.

--- lib/git/lint/analyzer.rb
+++ lib/git/lint/analyzer.rb
@@ -32,6 +32,7 @@ module Git
         Analyzers::CommitTrailerFormatValue,
         Analyzers::CommitTrailerIssueKey,
         Analyzers::CommitTrailerIssueValue,
+        Analyzers::CommitTrailerSignerKey,
         Analyzers::CommitTrailerTrackerKey,
         Analyzers::CommitTrailerTrackerValue
       ].freeze
