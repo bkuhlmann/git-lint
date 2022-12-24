61bf63ef26b819b4ebdffb536cc2b57fe2944a4e
Brooke Kuhlmann <brooke@alchemists.io>
2022-12-24 07:33:49 -0700

    Added container namespaces
    Necessary to include new low-level objects to aid in analyzing a
    repository. Each is broken down by namespace with some pulled in from
    the Gitt gem for improved reusability. This is the first step in
    upgrading this gem to leverage the new features provided by the Gitt
    gem.

--- lib/git/lint/container.rb
+++ lib/git/lint/container.rb
@@ -19,6 +19,29 @@ module Git
       register(:kernel) { Kernel }
       register(:executor) { Open3 }
       register(:logger) { Cogger::Client.new }
+
+      namespace :trailers do
+        register(:collaborator) { /\ACo.*Authored.*By.*\Z/i }
+        register(:format) { /\AFormat.*\Z/i }
+        register(:issue) { /\AIssue.*\Z/i }
+        register(:signer) { /\ASigned.*By.*\Z/i }
+        register(:tracker) { /\ATracker.*\Z/i }
+      end
+
+      namespace :parsers do
+        register(:person) { Gitt::Parsers::Person.new }
+      end
+
+      namespace :sanitizers do
+        register(:email) { Gitt::Sanitizers::Email }
+        register(:signature) { Gitt::Sanitizers::Signature }
+      end
+
+      namespace :validators do
+        register(:capitalization) { Validators::Capitalization }
+        register(:email) { Validators::Email }
+        register(:name) { Validators::Name }
+      end
     end
   end
 end
