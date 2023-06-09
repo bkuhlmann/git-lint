7874c34932281e77e08839f751cb32bbf51c83c1
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-09 16:11:24 -0600

    Updated container to use Etcher configuration
    Necessary to leverage Etcher and Runcom for loading and resolving the configuration.

--- lib/git/lint/container.rb
+++ lib/git/lint/container.rb
@@ -2,23 +2,37 @@
 
 require "cogger"
 require "dry-container"
+require "etcher"
 require "gitt"
+require "runcom"
 require "spek"
 require "tone"
 
 module Git
   module Lint
+    # :nocov:
     # Provides a global gem container for injection into other objects.
     module Container
       extend Dry::Container::Mixin
 
-      register(:configuration) { Configuration::Loader.call }
+      register :configuration do
+        self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+                       .then { |registry| Etcher.call registry }
+      end
+
+      register :defaults do
+        Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
+                        .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+      end
+
+      register(:color) { Tone.new }
       register(:environment) { ENV }
       register(:git) { Gitt::Repository.new }
+      register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
+      register(:xdg_config) { Runcom::Config.new "git-lint/configuration.yml" }
       register(:specification) { Spek::Loader.call "#{__dir__}/../../../git-lint.gemspec" }
       register(:kernel) { Kernel }
       register(:logger) { Cogger.new formatter: :emoji }
-      register(:color) { Tone.new }
 
       namespace :trailers do
         register(:collaborator) { /\ACo.*Authored.*By.*\Z/i }
--- spec/support/shared_contexts/application_dependencies.rb
+++ spec/support/shared_contexts/application_dependencies.rb
@@ -6,17 +6,23 @@ require "infusible/stub"
 RSpec.shared_context "with application dependencies" do
   using Infusible::Stub
 
+  include_context "with temporary directory"
+
   let :configuration do
-    YAML.load_file(Bundler.root.join("tmp/defaults.yml")).then do |settings|
-      Git::Lint::Configuration::Loader.new(client: settings).call
-    end
+    Etcher.new(Git::Lint::Container[:defaults])
+          .call(
+            commits_body_presence_enabled: false,
+            commits_signature_includes: %w[Good Invalid]
+          )
+          .bind(&:dup)
   end
 
   let(:environment) { Hash.new }
+  let(:xdg_config) { Runcom::Config.new Git::Lint::Container[:defaults_path] }
   let(:kernel) { class_spy Kernel }
-  let(:logger) { Cogger.new io: StringIO.new, formatter: :emoji }
+  let(:logger) { Cogger.new io: StringIO.new, level: :debug, formatter: :emoji }
 
-  before { Git::Lint::Import.stub configuration:, environment:, kernel:, logger: }
+  before { Git::Lint::Import.stub configuration:, environment:, xdg_config:, kernel:, logger: }
 
-  after { Git::Lint::Import.unstub :configuration, :environment, :kernel, :logger }
+  after { Git::Lint::Import.unstub :configuration, :environment, :xdg_config, :kernel, :logger }
 end
