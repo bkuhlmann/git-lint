f8de95fc62c3615747f31b28c7197cc723dde59e
Brooke Kuhlmann <brooke@alchemists.io>
2023-06-10 13:47:33 -0600

    Removed duplicated code from Sod upgrade
    None of this is necessary and can be safely deleted now that this project has been updated to run on Sod. Once again, this is another nice reduction in the amount of code to maintain.

--- spec/support/shared_examples/a_parser.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_examples "a parser" do
-  describe ".call" do
-    it "answers configuration" do
-      expect(described_class.call).to be_a(Git::Lint::Configuration::Model)
-    end
-  end
-end
