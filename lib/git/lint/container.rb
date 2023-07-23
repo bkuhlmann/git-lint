fed92d770e0b0fdb01bd6d3c8decb48470ffc838
Brooke Kuhlmann <brooke@alchemists.io>
2023-07-23 14:25:57 -0600

    Added container memoization to improve performance
    Necessary to improve CLI load time as well as the test suite by avoiding repeated loading of XDG configuration and associated objects.

--- lib/git/lint/container.rb
+++ lib/git/lint/container.rb
@@ -14,35 +14,16 @@ module Git
     module Container
       extend Dry::Container::Mixin
 
-      register :configuration do
-        self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
-                       .then { |registry| Etcher.call registry }
-      end
-
-      register :defaults do
-        Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
-                        .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
-      end
-
-      register(:color) { Tone.new }
-      register(:environment) { ENV }
-      register(:git) { Gitt::Repository.new }
-      register(:defaults_path) { Pathname(__dir__).join("configuration/defaults.yml") }
-      register(:xdg_config) { Runcom::Config.new "git-lint/configuration.yml" }
-      register(:specification) { Spek::Loader.call "#{__dir__}/../../../git-lint.gemspec" }
-      register(:kernel) { Kernel }
-      register(:logger) { Cogger.new formatter: :emoji }
-
       namespace :trailers do
-        register(:collaborator) { /\ACo.*Authored.*By.*\Z/i }
-        register(:format) { /\AFormat.*\Z/i }
-        register(:issue) { /\AIssue.*\Z/i }
-        register(:signer) { /\ASigned.*By.*\Z/i }
-        register(:tracker) { /\ATracker.*\Z/i }
+        register(:collaborator, memoize: true) { /\ACo.*Authored.*By.*\Z/i }
+        register(:format, memoize: true) { /\AFormat.*\Z/i }
+        register(:issue, memoize: true) { /\AIssue.*\Z/i }
+        register(:signer, memoize: true) { /\ASigned.*By.*\Z/i }
+        register(:tracker, memoize: true) { /\ATracker.*\Z/i }
       end
 
       namespace :parsers do
-        register(:person) { Gitt::Parsers::Person.new }
+        register(:person, memoize: true) { Gitt::Parsers::Person.new }
       end
 
       namespace :sanitizers do
@@ -51,10 +32,35 @@ module Git
       end
 
       namespace :validators do
-        register(:capitalization) { Validators::Capitalization.new }
-        register(:email) { Validators::Email.new }
-        register(:name) { Validators::Name.new }
+        register(:capitalization, memoize: true) { Validators::Capitalization.new }
+        register(:email, memoize: true) { Validators::Email.new }
+        register(:name, memoize: true) { Validators::Name.new }
       end
+
+      register :configuration, memoize: true do
+        self[:defaults].add_loader(Etcher::Loaders::YAML.new(self[:xdg_config].active))
+                       .then { |registry| Etcher.call registry }
+      end
+
+      register :defaults, memoize: true do
+        Etcher::Registry.new(contract: Configuration::Contract, model: Configuration::Model)
+                        .add_loader(Etcher::Loaders::YAML.new(self[:defaults_path]))
+      end
+
+      register :defaults_path, memoize: true do
+        Pathname(__dir__).join("configuration/defaults.yml")
+      end
+
+      register :specification, memoize: true do
+        Spek::Loader.call "#{__dir__}/../../../git-lint.gemspec"
+      end
+
+      register(:color, memoize: true) { Tone.new }
+      register(:environment) { ENV }
+      register(:git, memoize: true) { Gitt::Repository.new }
+      register(:xdg_config, memoize: true) { Runcom::Config.new "git-lint/configuration.yml" }
+      register(:logger, memoize: true) { Cogger.new formatter: :emoji }
+      register :kernel, Kernel
     end
   end
 end
