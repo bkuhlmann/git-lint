c470a5ffd2672691e5e5afac3df49d2dc3bfb751
Brooke Kuhlmann <brooke@alchemists.io>
2025-01-21 10:07:05 -0700

    Refactored collector to container
    Necessary to define this object once and use in multiple places. This will soon be applied to the analyzers as well.
    
    Milestone: patch

--- lib/git/lint/reporters/branch.rb
+++ lib/git/lint/reporters/branch.rb
@@ -5,14 +5,9 @@ module Git
     module Reporters
       # Reports issues related to a single branch.
       class Branch
-        include Dependencies[:color]
+        include Dependencies[:collector, :color]
 
         using Refinements::String
-
-        def initialize(collector: Collector.new, **)
-          super(**)
-          @collector = collector
-        end
 
         def to_s
           "Running Git Lint...#{branch_report}\n" \
@@ -22,8 +17,6 @@ module Git
         alias to_str to_s
 
         private
-
-        attr_reader :collector
 
         def branch_report
           return "" unless collector.issues?
