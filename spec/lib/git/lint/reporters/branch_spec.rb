c1164d01db0a491228ae6b6a2eab5be05c206401
Brooke Kuhlmann <brooke@alchemists.io>
2025-01-24 13:32:14 -0700

    Updated branch reporter to use delegates and totals
    The delegates are injected to avoid hard coding them while also distinguishing between single and group behavior.
    
    Necessary to improve performance and only compute totals once. The ability for the reporter to delegate to totals is included so we can avoid yielding the collector via the analyzer. We'll soon be able to yield only the reporter instead.
    
    Milestone: minor

--- spec/lib/git/lint/reporters/branch_spec.rb
+++ spec/lib/git/lint/reporters/branch_spec.rb
@@ -3,11 +3,36 @@
 require "spec_helper"
 
 RSpec.describe Git::Lint::Reporters::Branch do
-  subject(:reporter) { described_class.new collector: }
+  subject(:reporter) { described_class.new collector:, total: }
 
   include_context "with Git commit"
 
   let(:collector) { Git::Lint::Collector.new }
+  let(:total) { Git::Lint::Models::Total[items: 1] }
+
+  describe "#empty?" do
+    it "answers false" do
+      expect(reporter.empty?).to be(false)
+    end
+  end
+
+  describe "#issues?" do
+    it "answers false" do
+      expect(reporter.issues?).to be(false)
+    end
+  end
+
+  describe "#warnings?" do
+    it "answers false" do
+      expect(reporter.warnings?).to be(false)
+    end
+  end
+
+  describe "#errors?" do
+    it "answers false" do
+      expect(reporter.errors?).to be(false)
+    end
+  end
 
   describe "#to_s" do
     let :analyzer do
@@ -18,16 +43,15 @@ RSpec.describe Git::Lint::Reporters::Branch do
                    ),
                    commit: git_commit,
                    severity: "warn",
-                   invalid?: true,
-                   warning?: true,
-                   error?: false,
                    issue: {hint: "A test hint."}
     end
 
-    context "with warnings" do
-      before { collector.add analyzer }
+    context "with individual warnings" do
+      let(:total) { Git::Lint::Models::Total[items: 1, issues: 1, warnings: 1] }
 
-      it "answers detected issues" do
+      it "answers totals" do
+        collector.add analyzer
+
         expect(reporter.to_s).to eq(
           "Running Git Lint...\n" \
           "\n" \
@@ -40,7 +64,9 @@ RSpec.describe Git::Lint::Reporters::Branch do
       end
     end
 
-    context "with errors" do
+    context "with individual errors" do
+      let(:total) { Git::Lint::Models::Total[items: 1, issues: 1, errors: 1] }
+
       let :analyzer do
         instance_spy Git::Lint::Analyzers::CommitAuthorEmail,
                      class: class_spy(
@@ -49,32 +75,56 @@ RSpec.describe Git::Lint::Reporters::Branch do
                      ),
                      commit: git_commit,
                      severity: "error",
-                     invalid?: true,
-                     warning?: false,
-                     error?: true,
                      issue: {hint: "A test hint."}
       end
 
-      before do
-        collector.add analyzer
-        collector.add analyzer
-      end
-
       it "answers detected issues" do
+        collector.add analyzer
+
         expect(reporter.to_s).to eq(
           "Running Git Lint...\n" \
           "\n" \
           "180dec7d8ae8cbe3565a727c63c2111e49e0b737 (Test User, 1 day ago): Added documentation\n" \
           "\e[31m  Commit Author Email Error. A test hint.\n\e[0m" \
-          "\e[31m  Commit Author Email Error. A test hint.\n\e[0m" \
           "\n" \
-          "1 commit inspected. \e[31m2 issues\e[0m detected " \
-          "(\e[32m0 warnings\e[0m, \e[31m2 errors\e[0m).\n"
+          "1 commit inspected. \e[31m1 issue\e[0m detected " \
+          "(\e[32m0 warnings\e[0m, \e[31m1 error\e[0m).\n"
+        )
+      end
+    end
+
+    context "with group errors" do
+      let(:total) { Git::Lint::Models::Total[items: 1, issues: 1, errors: 1] }
+
+      let :analyzer do
+        instance_spy Git::Lint::Analyzers::Commits::Subjects::Duplicate,
+                     class: class_spy(
+                       Git::Lint::Analyzers::Commits::Subjects::Duplicate,
+                       label: "Commit Subject Duplicate"
+                     ),
+                     commit: nil,
+                     commits: [git_commit, git_commit],
+                     severity: "error",
+                     issue: {hint: "A test hint."}
+      end
+
+      it "answers detected issues" do
+        collector.add analyzer
+
+        expect(reporter.to_s).to eq(
+          "Running Git Lint...\n" \
+          "\n" \
+          "\e[31mCommit Subject Duplicate Error. A test hint.\n\e[0m" \
+          "\n" \
+          "1 commit inspected. \e[31m1 issue\e[0m detected " \
+          "(\e[32m0 warnings\e[0m, \e[31m1 error\e[0m).\n"
         )
       end
     end
 
     context "without issues" do
+      let(:total) { Git::Lint::Models::Total.new }
+
       it "answers zero detected issues" do
         expect(reporter.to_s).to eq(
           "Running Git Lint...\n" \
@@ -85,6 +135,8 @@ RSpec.describe Git::Lint::Reporters::Branch do
   end
 
   describe "#to_str" do
+    let(:total) { Git::Lint::Models::Total.new }
+
     it "answers implicit string" do
       expect(reporter.to_str).to eq(
         "Running Git Lint...\n" \
